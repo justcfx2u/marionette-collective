fd3e1b035ed9842c542c28c32e68530157c3a12f
H. Arnold Jones <github-commits@cfxmusic.com>
2012-02-16 16:01:34 -0500

    Initial work to daemonize mcollective on Windows.
    Requires a service to be configured prior to launch.
    
    Assuming:
    C:\dev\marionette-collective\bin\mcollectived
    
    Then:
    sc create mcollective binPath= "C:\Ruby187\bin\rubyw.exe -C C:\dev\
    marionette-collective\bin\mcollectived --config C:\dev\mc-server.cfg"
    type= own start= demand
    
    ** NOTE spaces are extremely significant in the above command!
    
    Service control:
    net start mcollective
    net stop mcollective

---
+++ lib/mcollective/runnerwindows.rb
@@ -0,0 +1,136 @@
+module MCollective
+  # The main runner for the daemon, supports running in the foreground
+  # and the background, keeps detailed stats and provides hooks to access
+  # all this information
+  
+  require 'win32/daemon'
+  include Win32
+  
+  class Runner < Daemon
+    
+    def service_init(*args)
+      Log.debug("service_init start")
+      @config = Config.instance
+      @config.loadconfig(configfile) unless @config.configured
+
+      @stats = PluginManager["global_stats"]
+
+      @security = PluginManager["security_plugin"]
+      @security.initiated_by = :node
+
+      @connection = PluginManager["connector_plugin"]
+      Log.debug("service_init end")
+    end
+
+    # Starts the main loop, before calling this you should initialize the MCollective::Config singleton.
+    def service_main
+      Log.debug('service_main start')
+      @connection.connect
+
+      @agents = Agents.new
+      
+      Util.subscribe(Util.make_subscriptions("mcollective", :broadcast))
+      Util.subscribe(Util.make_subscriptions("mcollective", :directed)) if @config.direct_addressing
+
+      # Start the registration plugin if interval isn't 0
+      begin
+        PluginManager["registration_plugin"].run(@connection) unless @config.registerinterval == 0
+      rescue Exception => e
+        Log.error("Failed to start registration plugin: #{e}")
+      end
+
+      while running?
+        begin
+          request = receive
+
+          if request.agent == "mcollective"
+            controlmsg(request)
+          else
+            agentmsg(request)
+          end
+        rescue Interrupt
+          Log.warn("Exiting after interrupt signal")
+          @connection.disconnect
+          exit!
+
+        rescue MsgTTLExpired => e
+          Log.warn(e)
+
+        rescue NotTargettedAtUs => e
+          Log.debug("Message does not pass filters, ignoring")
+
+        rescue Exception => e
+          Log.warn("Failed to handle message: #{e} - #{e.class}\n")
+          Log.warn(e.backtrace.join("\n\t"))
+        end
+      end
+      
+      Log.debug('service_main end')
+    end
+
+    def service_stop
+      Log.warn("Exiting after service stop request")
+      @connection.disconnect
+      exit!
+    end
+    
+    private
+    # Deals with messages directed to agents
+    def agentmsg(request)
+      Log.debug("Handling message for agent '#{request.agent}' on collective '#{request.collective}'")
+
+      @agents.dispatch(request, @connection) do |reply_message|
+        reply(reply_message, request) if reply_message
+      end
+    end
+
+    # Deals with messages sent to our control topic
+    def controlmsg(request)
+      Log.debug("Handling message for mcollectived controller")
+
+      begin
+        case request.payload[:body]
+        when /^stats$/
+          reply(@stats.to_hash, request)
+
+        when /^reload_agent (.+)$/
+          reply("reloaded #{$1} agent", request) if @agents.loadagent($1)
+
+        when /^reload_agents$/
+
+          reply("reloaded all agents", request) if @agents.loadagents
+
+        else
+          Log.error("Received an unknown message to the controller")
+
+        end
+      rescue Exception => e
+        Log.error("Failed to handle control message: #{e}")
+      end
+    end
+
+    # Receive a message from the connection handler
+    def receive
+      request = @connection.receive
+      request.type = :request
+
+      @stats.received
+
+      request.decode!
+      request.validate
+
+      request
+    end
+
+    # Sends a reply to a specific target topic
+    def reply(msg, request)
+      msg = Message.new(msg, nil, :request => request)
+      msg.encode!
+      msg.publish
+
+      @stats.sent
+    end
+    
+  end
+end
+
